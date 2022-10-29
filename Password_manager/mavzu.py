47a319a4758dfdd15f41d084c74e34e0e4be1c79
jony-sayfullaev <jonicks2000@gmail.com>
2022-10-29 00:07:24 -0400

    password manager GUI application with python built-in JSON package

---
+++ Password_manager/mavzu.py
@@ -0,0 +1,8 @@
+"""JSON=> Javascript object Notation
+data formatter = easy way to trasferring data accross the internet
+* To work with JSON data in python we use built-in JSON library
+WRITE = json.dump()
+READ = json.load()
+UPDATE = json.update()
+
+"""
