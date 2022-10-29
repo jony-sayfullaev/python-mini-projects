3ecf6a96a4525d598acfd5fa576fc2a183b3c7e0
jony-sayfullaev <jonicks2000@gmail.com>
2022-10-28 22:42:49 -0400

    code updated with pyhton  Error Handling

--- nato_alphabet_start/main.py
+++ nato_alphabet_start/main.py
@@ -4,6 +4,17 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def phonetic_func():
+    word = input("Enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        phonetic_func()
+    else:
+        print(output_list)
+
+
+phonetic_func()
